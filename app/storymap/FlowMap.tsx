365bffcd010a6fad8a99d5080520368cca1319a5
Praneeth Ravuri <pravdevrav@gmail.com>
2024-07-25 16:17:43 -0700

    style: added theme room details in hover card

--- app/storymap/FlowMap.tsx
+++ app/storymap/FlowMap.tsx
@@ -9,11 +9,12 @@ import {
   addEdge,
   Handle,
   Position,
+  Node,
+  Edge,
 } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
 import { useTheme } from "next-themes";
 import NodeCard from './NodeCard';
-import ControlPanel from './ControlPanel';
 import { useRouter, useSearchParams } from 'next/navigation';
 import LoadingScreen from '@/components/LoadingScreen';
 import {
@@ -21,9 +22,11 @@ import {
   HoverCardContent,
   HoverCardTrigger,
 } from "@/components/ui/hover-card"
+import { Button } from "@/components/ui/button";
+import { CalendarIcon, TagIcon } from 'lucide-react';
 
-
-export interface NodeData {
+// Interfaces
+interface NodeData {
   title: string;
   createdAt: string;
   author: string;
@@ -43,29 +46,60 @@ interface ThemeRoom {
   tags: string[];
 }
 
+interface HoverCardComponentProps {
+  themeRoomDetails: ThemeRoom;
+  nodesLength: number;
+}
+
+// Custom Node Component
 const CustomNode: React.FC<{ data: NodeData }> = ({ data }) => {
   const { theme } = useTheme();
   const isDark = theme === 'dark';
+
   return (
     <>
       <Handle type="target" position={Position.Top} className="!bg-transparent" />
-      <NodeCard
-        title={data.title}
-        createdAt={data.createdAt}
-        author={data.author}
-        isDark={isDark}
-        currId={data.id}
-        isSelected={data.isSelected}
-        isFavorited={data.isFavorited}
-        onSelect={data.onSelect}
-        onFavorite={data.onFavorite}
-        themeRoomId={data.themeRoomId}
-      />
+      <NodeCard {...data} isDark={isDark} currId={data.id} />
       <Handle type="source" position={Position.Bottom} className="!bg-transparent" />
     </>
   );
 };
 
+// HoverCard Component
+const HoverCardComponent: React.FC<HoverCardComponentProps> = ({ themeRoomDetails, nodesLength }) => (
+  <div className="absolute top-4 right-4 z-10">
+    <HoverCard>
+      <HoverCardTrigger asChild>
+        <button  className="btn btn-primary">
+          {themeRoomDetails.name} 
+        </button>
+      </HoverCardTrigger>
+      <HoverCardContent className="w-80 p-4" side="left">
+        <div className="space-y-2">
+          <h3 className="text-lg font-semibold">{themeRoomDetails.name} (<span>{nodesLength}</span>)</h3>
+          <p className="text-sm text-muted-foreground">{themeRoomDetails.description}</p>
+          {themeRoomDetails.tags.length > 0 && (
+            <div className="flex flex-wrap gap-1 mt-2 items-center">
+              <TagIcon className="h-3 w-3 text-muted-foreground" />
+              {themeRoomDetails.tags.map(tag => (
+                <span key={tag} className="text-xs px-2 py-1 bg-secondary text-secondary-foreground rounded-full">
+                  {tag}
+                </span>
+              ))}
+            </div>
+          )}
+          {nodesLength === 0 && (
+            <p className="text-sm text-muted-foreground mt-2">
+              Currently, there are no stories in this flow map. Create a root story to get started!
+            </p>
+          )}
+        </div>
+      </HoverCardContent>
+    </HoverCard>
+  </div>
+);
+
+// Main FlowMap Component
 export default function FlowMap() {
   const [nodes, setNodes, onNodesChange] = useNodesState([]);
   const [edges, setEdges, onEdgesChange] = useEdgesState([]);
@@ -74,20 +108,21 @@ export default function FlowMap() {
   const [selectedStoryIds, setSelectedStoryIds] = useState(new Set<string>());
   const [favoritedStoryIds, setFavoritedStoryIds] = useState(new Set<string>());
   const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [themeRoomDetails, setThemeRoomDetails] = useState<ThemeRoom[]>([]);
-
+  const [themeRoomDetails, setThemeRoomDetails] = useState<ThemeRoom | null>(null);
 
   const router = useRouter();
   const searchParams = useSearchParams();
   const themeRoomId = searchParams.get('themeRoomId');
 
-  const selectedStories = useMemo(() => {
-    return nodes.filter(node => selectedStoryIds.has(node.id)).map(node => node.data);
-  }, [nodes, selectedStoryIds]);
+  const selectedStories = useMemo(() => 
+    nodes.filter(node => selectedStoryIds.has(node.id)).map(node => node.data),
+    [nodes, selectedStoryIds]
+  );
 
-  const favoritedStories = useMemo(() => {
-    return nodes.filter(node => favoritedStoryIds.has(node.id)).map(node => node.data);
-  }, [nodes, favoritedStoryIds]);
+  const favoritedStories = useMemo(() => 
+    nodes.filter(node => favoritedStoryIds.has(node.id)).map(node => node.data),
+    [nodes, favoritedStoryIds]
+  );
 
   useEffect(() => {
     const updateScreenSize = () => {
@@ -103,97 +138,54 @@ export default function FlowMap() {
   const onSelect = useCallback((id: string) => {
     setSelectedStoryIds(prev => {
       const newSet = new Set(prev);
-      if (newSet.has(id)) {
-        newSet.delete(id);
-      } else {
-        newSet.add(id);
-      }
+      newSet.has(id) ? newSet.delete(id) : newSet.add(id);
       return newSet;
     });
 
     setNodes(nds =>
-      nds.map(node => {
-        if (node.id === id) {
-          return {
-            ...node,
-            data: {
-              ...node.data,
-              isSelected: !node.data.isSelected,
-            },
-          };
-        }
-        return node;
-      })
+      nds.map(node => node.id === id ? { ...node, data: { ...node.data, isSelected: !node.data.isSelected } } : node)
     );
   }, [setNodes]);
 
   const onFavorite = useCallback((id: string) => {
     setFavoritedStoryIds(prev => {
       const newSet = new Set(prev);
-      if (newSet.has(id)) {
-        newSet.delete(id);
-      } else {
-        newSet.add(id);
-      }
+      newSet.has(id) ? newSet.delete(id) : newSet.add(id);
       return newSet;
     });
 
     setNodes(nds =>
-      nds.map(node => {
-        if (node.id === id) {
-          return {
-            ...node,
-            data: {
-              ...node.data,
-              isFavorited: !node.data.isFavorited,
-            },
-          };
-        }
-        return node;
-      })
+      nds.map(node => node.id === id ? { ...node, data: { ...node.data, isFavorited: !node.data.isFavorited } } : node)
     );
   }, [setNodes]);
 
   const removeStory = useCallback((id: string, type: 'selected' | 'favorited') => {
-    if (type === 'selected') {
-      setSelectedStoryIds(prev => {
-        const newSet = new Set(prev);
-        newSet.delete(id);
-        return newSet;
-      });
-    } else {
-      setFavoritedStoryIds(prev => {
-        const newSet = new Set(prev);
-        newSet.delete(id);
-        return newSet;
-      });
-    }
+    const setFunc = type === 'selected' ? setSelectedStoryIds : setFavoritedStoryIds;
+    setFunc(prev => {
+      const newSet = new Set(prev);
+      newSet.delete(id);
+      return newSet;
+    });
 
     setNodes(nds =>
-      nds.map(node => {
-        if (node.id === id) {
-          return {
-            ...node,
-            data: {
-              ...node.data,
-              [type === 'selected' ? 'isSelected' : 'isFavorited']: false,
-            },
-          };
-        }
-        return node;
-      })
+      nds.map(node => node.id === id ? { ...node, data: { ...node.data, [type === 'selected' ? 'isSelected' : 'isFavorited']: false } } : node)
     );
   }, [setNodes]);
 
   useEffect(() => {
-    const fetchStories = async () => {
+    const fetchData = async () => {
       if (!themeRoomId) return;
 
+      setIsLoading(true);
       try {
-        setIsLoading(true);
-        const response = await fetch(`/api/theme-room-api/fetch-all-stories?themeRoomId=${themeRoomId}`);
-        const stories = await response.json();
-        console.log(JSON.stringify(stories))
+        const [storiesResponse, themeRoomResponse] = await Promise.all([
+          fetch(`/api/theme-room-api/fetch-all-stories?themeRoomId=${themeRoomId}`),
+          fetch(`/api/theme-room-api/fetch-one?themeRoomId=${themeRoomId}`)
+        ]);
+
+        const stories = await storiesResponse.json();
+        const themeRoom = await themeRoomResponse.json();
+
         const { constructedNodes, constructedEdges } = constructNodesAndEdges(stories, screenSize);
 
         setNodes(constructedNodes.map(node => ({
@@ -202,140 +194,77 @@ export default function FlowMap() {
             ...node.data,
             isSelected: false,
             isFavorited: false,
-            onSelect: onSelect,
-            onFavorite: onFavorite,
-            themeRoomId: themeRoomId
+            onSelect,
+            onFavorite,
+            themeRoomId
           }
         })));
 
         setEdges(constructedEdges);
+        setThemeRoomDetails(themeRoom);
       } catch (error) {
-        console.error('Error fetching stories:', error);
+        console.error('Error fetching data:', error);
       } finally {
-        setIsLoading(false)
+        setIsLoading(false);
       }
     };
 
-    fetchStories();
+    fetchData();
   }, [screenSize, setNodes, setEdges, onSelect, onFavorite, themeRoomId]);
-
-  useEffect(() => {
-    const fetchThemeRoomDetails = async () => {
-      try {
-        setIsLoading(true)
-        const response = await fetch(`/api/theme-room-api/fetch-one?themeRoomId=${themeRoomId}`);
-        if (response.ok) {
-          const themeRoom = await response.json();
-          setThemeRoomDetails(themeRoom)
-        }
-      } catch (error) {
-        console.error('Error fetching theme room details:', error)
-      } finally {
-        setIsLoading(false)
-      }
-    }
-
-    fetchThemeRoomDetails();
-
-  }, [themeRoomId])
 
   const onConnect = useCallback(
     (params) => setEdges((eds) => addEdge(params, eds)),
     [setEdges],
   );
 
-  const nodeTypes = {
-    custom: CustomNode,
-  };
+  const nodeTypes = { custom: CustomNode };
 
-  // const HoverCardComponent = () => (
-  //   <div className="absolute top-4 right-4 z-10">
-  //     <HoverCard>
-  //       <HoverCardTrigger asChild>
-  //         <button className="px-4 py-2 btn btn-primary">
-  //           {themeRoomDetails.name}
-  //         </button>
-  //       </HoverCardTrigger>
-  //       <HoverCardContent className="w-80">
-  //         <h3 className="text-lg font-semibold">{themeRoomDetails.name}</h3>
-  //         <p className="text-sm text-gray-600">{themeRoomDetails.description}</p>
-  //         <p className="mt-2 text-sm text-gray-500">Created on: {new Date(themeRoomDetails.createdAt).toLocaleDateString()}</p>
-  //         <p className="text-sm text-gray-500">Last updated: {new Date(themeRoomDetails.updatedAt).toLocaleDateString()}</p>
-  //         {themeRoomDetails.tags.length > 0 && (
-  //           <div className="mt-2">
-  //             <span className="font-semibold">Tags:</span>
-  //             <ul className="list-disc ml-4 text-sm text-gray-600">
-  //               {themeRoomDetails.tags.map(tag => (
-  //                 <li key={tag}>{tag}</li>
-  //               ))}
-  //             </ul>
-  //           </div>
-  //         )}
-  //         {nodes.length === 0 && (
-  //           <p className="mt-2 text-sm text-gray-500">Currently, there are no stories in this flow map. Create a root story to get started!</p>
-  //         )}
-  //       </HoverCardContent>
-  //     </HoverCard>
-  //   </div>
-  // );
-
+  if (isLoading) return <LoadingScreen />;
 
   return (
-    isLoading ? (
-      <LoadingScreen />
-    ) : (
-      <div className="h-screen relative">
-        {/* <HoverCardComponent /> */}
-        {nodes.length === 0 ? (
-          <div className="flex flex-col items-center justify-center h-full">
-            <p className="text-2xl mb-4">No stories found.</p>
-            <button
-              className="btn btn-primary"
-              onClick={() => router.push(`/create-story?type=root&themeRoomId=${themeRoomId}`)}
-            >
-              Create a Root Story
-            </button>
-          </div>
-        ) : (
-          <div style={{ width: '100vw', height: '100vh', overflow: 'hidden' }}>
-            <ReactFlow
-              nodes={nodes}
-              edges={edges}
-              onNodesChange={onNodesChange}
-              onEdgesChange={onEdgesChange}
-              onConnect={onConnect}
-              fitView
-              nodeTypes={nodeTypes}
-              colorMode={theme === "dark" ? "dark" : "light"}
-            >
-              <Controls />
-              <Background gap={12} size={1} />
-            </ReactFlow>
-          </div>
-        )}
-      </div>
-    )
+    <div className="h-screen relative">
+      {themeRoomDetails && <HoverCardComponent themeRoomDetails={themeRoomDetails} nodesLength={nodes.length} />}
+      {nodes.length === 0 ? (
+        <div className="flex flex-col items-center justify-center h-full">
+          <p className="text-2xl mb-4">No stories found.</p>
+          <Button onClick={() => router.push(`/create-story?type=root&themeRoomId=${themeRoomId}`)}>
+            Create a Root Story
+          </Button>
+        </div>
+      ) : (
+        <div style={{ width: '100vw', height: '100vh', overflow: 'hidden' }}>
+          <ReactFlow
+            nodes={nodes}
+            edges={edges}
+            onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            onConnect={onConnect}
+            fitView
+            nodeTypes={nodeTypes}
+            colorMode={theme === "dark" ? "dark" : "light"}
+          >
+            <Controls />
+            <Background gap={12} size={1} />
+          </ReactFlow>
+        </div>
+      )}
+    </div>
   );
 }
 
-
-
 function constructNodesAndEdges(stories: any[], screenSize: { width: number, height: number }) {
-  const constructedNodes: any[] = [];
-  const constructedEdges: any[] = [];
+  const constructedNodes: Node[] = [];
+  const constructedEdges: Edge[] = [];
   const nodeWidth = 200;
   const nodeHeight = 100;
   const horizontalSpacing = Math.max(nodeWidth * 3, screenSize.width * 0.1);
   const verticalSpacing = Math.max(nodeHeight * 5, screenSize.height * 0.15);
 
-  // Find the root node
   const rootNode = stories.find(story => story.type === 'root');
   if (!rootNode) return { constructedNodes, constructedEdges };
 
-  // Helper function to get children of a node
   const getChildren = (nodeId: string) => stories.filter(story => story.prev.includes(nodeId));
 
-  // Helper function to get the maximum depth of the tree
   const getMaxDepth = (node: any, depth = 0): number => {
     const children = getChildren(node.customId);
     if (children.length === 0) return depth;
@@ -343,29 +272,22 @@ function constructNodesAndEdges(stories: any[], screenSize: { width: number, hei
   };
 
   const maxDepth = getMaxDepth(rootNode) + 1;
-  console.log(`Depth: ${maxDepth}`);
 
-  // Create a map to store nodes at each level
   const levelMap: Map<number, any[]> = new Map();
 
-  // Helper function to populate the level map
   function populateLevelMap(node: any, level: number) {
     if (!levelMap.has(level)) {
       levelMap.set(level, []);
     }
     levelMap.get(level)!.push(node);
 
-    const children = getChildren(node.customId);
-    children.forEach(child => populateLevelMap(child, level + 1));
+    getChildren(node.customId).forEach(child => populateLevelMap(child, level + 1));
   }
 
-  // Populate the level map starting from the root
   populateLevelMap(rootNode, 0);
 
-  // Helper function to recursively position nodes
   function positionNodes(node: any, level: number, index: number) {
     const nodesAtLevel = levelMap.get(level)!;
-    const levelWidth = (nodesAtLevel.length - 1) * horizontalSpacing;
     const x = (index - (nodesAtLevel.length - 1) / 2) * horizontalSpacing;
     const y = level * verticalSpacing;
 
@@ -381,7 +303,6 @@ function constructNodesAndEdges(stories: any[], screenSize: { width: number, hei
       type: 'custom'
     });
 
-    // Create edges
     node.prev.forEach((prevId: string) => {
       constructedEdges.push({
         id: `e-${prevId}-${node.customId}`,
@@ -391,24 +312,18 @@ function constructNodesAndEdges(stories: any[], screenSize: { width: number, hei
       });
     });
 
-    // Position child nodes
-    const children = getChildren(node.customId);
-    children.forEach(child => {
+    getChildren(node.customId).forEach((child, childIndex) => {
       const childLevel = level + 1;
-      const childIndex = levelMap.get(childLevel)!.indexOf(child);
       positionNodes(child, childLevel, childIndex);
     });
   }
 
-  // Start positioning from the root node
   positionNodes(rootNode, 0, 0);
 
-  // Calculate the bounding box of all nodes
   const minX = Math.min(...constructedNodes.map(node => node.position.x));
   const maxX = Math.max(...constructedNodes.map(node => node.position.x));
   const totalWidth = maxX - minX + nodeWidth;
 
-  // Center the entire tree
   const xOffset = (screenSize.width - totalWidth) / 2 - minX;
   constructedNodes.forEach(node => {
     node.position.x += xOffset;
